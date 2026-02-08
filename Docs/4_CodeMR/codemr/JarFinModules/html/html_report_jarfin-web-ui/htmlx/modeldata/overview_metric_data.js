ffc73b9eda69f143bd89269ce33cc10d4065db85
alespix <a.rocco3@studenti.unibg.it>
2026-02-08 15:21:56 +0100

    Analisi statica aggiornata
    Rimpiazzati i grafici generati da CodeMR con una versione del codice aggiornata

---
+++ Docs/4_CodeMR/codemr/JarFinModules/html/html_report_jarfin-web-ui/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"analytics-service working set: {jarfin-web-ui}", "Number of Highly Problematic Classes":0, "Number of Entities":7, "Number of Problematic Classes":0, "Class Lines of Code":324, "Number of External Packages":12, "Number of Packages":4, "Number of External Entities":28}
